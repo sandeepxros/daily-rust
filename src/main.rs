56dcbe0c87c690409f138eccd6a1b0994e8284a5
Sandeep Soni <sandeepkumar8842@gmail.com>
2024-01-02 07:28:52 +0530

    day-2 done

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,90 @@
 #![allow(unused)]
 
+use std::cmp::Ordering;
 use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 use std::num::ParseIntError;
+
+use rand::Rng;
 fn main() {
-    //type string
-    const MYNAME: &str = "sandeep";
-    //type number
-    const MY_NUM: u32 = 1_000_000;
+    
+    // maths operation today we are going to read
 
-    const PI: f32 = 3.141592;
+    //decimal percision
+    let num_1: f32 = 1.1111111111;
+    let num_2: f64 = 1.1111111111;
+    println!("f32: {} ", num_1 + 0.1111111111);
+    println!("f64: {} ", num_2 + 0.1111111111);
 
-    let myage = "28232";
+    //arithmatic operations;
 
-    let mut myage: u32 = myage.trim().parse().expect("sorry can't convert it");
-    myage = myage + 1;
-    println!("{} and myage  {} ", MYNAME, myage);
+    let mut num3: i32 = 12;
+    let num4: i32 = 5;
 
+    println!("12 + 5 = {}", num3 + num4);
+    println!("12 - 5 = {}", num3 - num4);
+    println!("12 * 5 = {}", num3 * num4);
+    println!("12 / 5 = {}", num3 / num4);
+    println!("12 % 5 = {}", num3 % num4);
+    num3 += 1;
+    println!("num3+= : {}", num3);
+    num3 *= 2;
+    println!("num3*= : {}", num3);
+    num3 /= 2;
+    println!("num3/= : {}", num3);
+    num3 -= 5;
+    println!("num3-= : {}", num3);
+    num3 %= 5;
+    println!("num3%= : {}", num3);
 
-    println!("Unsigned Interget u32 Max: {}, Min : {}", u32::MAX, u32::MIN);
-
-    println!("signed Interget i32 Max: {}, Min : {}", i32::MAX, i32::MIN);
-
-    println!("max usize {} , min: {}", usize::MAX, usize::MIN);
-
-    let is_true = true;
-    let _istrue = true; // unused variable if you want to give
-
-    let my_grade = 'A';
+    //random number
+    let rand = rand::thread_rng().gen_range(1..101);
+    println!("Generated random number: {} ", rand);
 
     
 
+    //conditionals
+    let mut age = -101;
 
+    /*
+    if (age > 16) && (age < 14) {
+        println!("Congrats you can come into party.");
+    } else if (age > 19) || (age > 17) {
+        println!("please don't come without gifts");
+    } else if age == 12 {
+        println!("sorry you can't enter");
+    } else {
+        println!("go home buddy");
+    }
+     */
 
+    //ternary equivalent
+    /*
+    let mut is_Adult:bool = if age>=18 {
+        true
+    }else {
+        false
+    };
+    println!(" is it an adult : {}", is_Adult)
+    */
+
+    /*
+    //match statement
+
+    match age {
+        1..=17 => println!("You are bw 1,18"),
+        18 | 20 => println!("you are an adult"),
+        18..=50 => println!("you are a parent"),
+        18..=i32::MAX => println!("I dont know"),
+        _ => println!("whatever"),
+    }
+    */
+
+    // match with cmp and ordering
+    let voting_age = 18;
+    match age.cmp(&voting_age) {
+        Ordering::Less => println!("you can't vote"),
+        Ordering::Greater => println!("you can vote"),
+        Ordering::Equal => println!("you can start voting"),
+    }
 }
