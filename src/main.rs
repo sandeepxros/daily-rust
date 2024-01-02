04bd05b1b139e5a3194e4378f85c689ab395ed15
Sandeep Soni <sandeepkumar8842@gmail.com>
2024-01-02 06:33:15 +0530

    day 1

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,35 @@
 
 use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
+use std::num::ParseIntError;
 fn main() {
-    println!("What is your name?");
-    let mut name: String = String::new();
-    let greeting: &str = "Nice To Meet You";
-    io::stdin()
-        .read_line(&mut name)
-        .expect("You dind't enterd your name");
+    //type string
+    const MYNAME: &str = "sandeep";
+    //type number
+    const MY_NUM: u32 = 1_000_000;
 
-    println!("Hello, {},  {}", name.trim_end(), greeting);
+    const PI: f32 = 3.141592;
+
+    let myage = "28232";
+
+    let mut myage: u32 = myage.trim().parse().expect("sorry can't convert it");
+    myage = myage + 1;
+    println!("{} and myage  {} ", MYNAME, myage);
+
+
+    println!("Unsigned Interget u32 Max: {}, Min : {}", u32::MAX, u32::MIN);
+
+    println!("signed Interget i32 Max: {}, Min : {}", i32::MAX, i32::MIN);
+
+    println!("max usize {} , min: {}", usize::MAX, usize::MIN);
+
+    let is_true = true;
+    let _istrue = true; // unused variable if you want to give
+
+    let my_grade = 'A';
+
+    
+
+
+
 }
