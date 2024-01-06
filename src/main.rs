48d46b5bceed7fcf2123157151510abf65751759
Sandeep Soni <sandeepkumar8842@gmail.com>
2024-01-06 07:19:44 +0530

    day 5 done

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 #![allow(unused)]
 
+use std::collections::HashMap;
 use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
-fn main() {
-    println!("What is your name?");
-    let mut name: String = String::new();
-    let greeting: &str = "Nice To Meet You";
-    io::stdin()
-        .read_line(&mut name)
-        .expect("You dind't enterd your name");
 
-    println!("Hello, {},  {}", name.trim_end(), greeting);
-    
+fn sum(list:&[i32])->i32{
+    let mut val = 0; 
+    for item in list.iter(){
+        val+=item; 
+    }
+    val
+}
+
+fn check(val:i32) -> i32{
+    val+22
+}
+
+
+fn main() {
+    //generics 
+
+    let mut movies_and_hero = HashMap::new();
+    movies_and_hero.insert("Animal", "Ranbir");
+    movies_and_hero.insert("Robot", "RajniKanth");
+    movies_and_hero.insert("Iron Man", "RDJ");
+
+    for(k, v) in movies_and_hero.iter(){
+        println!("{} movie has {}", k, v);
+    }
+
+    println!("{}", movies_and_hero.get(&&"Animal"))
 }
